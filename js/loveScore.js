207214e516a091d11d815b7cb8712a3d5566d613
tamdinhkwende <52424302+tamdinhkwende@users.noreply.github.com>
2019-07-18 08:36:36 -0700

    Add files via upload

---
+++ js/loveScore.js
@@ -0,0 +1,11 @@
+prompt("What is your name?")
+prompt("What is their name?")
+var loveScore = Math.random() * 100;
+lovescore = Math.floor(loveScore) + 1;
+alert("your love score is " + loveScore + "%")
+
+
+var n = Math.random();
+n = n * 6;
+n = Math.floor(n) + 1;
+console.log(n);
